dc75a0ab962a51f1749c4dbc93e7d6befa404a01
Yelyzaveta Krasiuk <lizakrasyuk24@gmail.com>
2025-10-20 11:42:27 +0200

    init: init repo

---
+++ src/main/java/com/yely/bartrack_backend/BartrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yely.bartrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BartrackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yely/bartrack_backend/BartrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yely.bartrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
