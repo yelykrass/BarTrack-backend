93e345ecd55989d7f521b23cb39722e20f1db08c
Yelyzaveta Krasiuk <lizakrasyuk24@gmail.com>
2025-10-21 12:50:19 +0200

    add properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+/* Roles */
+INSERT INTO roles (id_role, name) VALUES (default, 'ROLE_ADMIN');
+INSERT INTO roles (id_role, name) VALUES (default, 'ROLE_USER');
+
+/* Users (bcrypt */
+INSERT INTO users (id_user, email, password) 
+VALUES (default, 'admin@bartrack.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO'); -- пароль: 123456
+INSERT INTO users (id_user, email, password) 
+VALUES (default, 'bartender@bartrack.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO'); -- пароль: 123456
+
+/* Roles_users */
+INSERT INTO roles_users (role_id, user_id) VALUES (1, 1); -- admin -> ROLE_ADMIN
+INSERT INTO roles_users (role_id, user_id) VALUES (2, 2); -- bartender -> ROLE_USER
+
+/* Bebidas (productos para control de existencias) */
+/*INSERT INTO drinks (id_drink, name, price, stock_quantity) VALUES (default, 'Mojito', 7.50, 20);
+INSERT INTO drinks (id_drink, name, price, stock_quantity) VALUES (default, 'Margarita', 8.00, 15);
+INSERT INTO drinks (id_drink, name, price, stock_quantity) VALUES (default, 'Cerveza', 4.50, 50);
+INSERT INTO drinks (id_drink, name, price, stock_quantity) VALUES (default, 'Vino tinto', 6.00, 30);
+*/
