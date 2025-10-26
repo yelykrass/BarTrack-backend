c7ca995f71aca3396a6b19aba664286ae2137323
Yelyzaveta Krasiuk <lizakrasyuk24@gmail.com>
2025-10-26 13:33:10 +0100

    add auth controller and auth dto response

---
+++ src/main/java/com/yely/bartrack_backend/auth/AuthController.java
@@ -0,0 +1,27 @@
+package com.yely.bartrack_backend.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "${api-endpoint}")
+public class AuthController {
+
+    @GetMapping("/login")
+    public ResponseEntity<AuthDTOResponse> login() {
+
+        SecurityContext contextHolder = SecurityContextHolder.getContext();
+        Authentication auth = contextHolder.getAuthentication();
+
+        AuthDTOResponse authResponse = new AuthDTOResponse("Logged", auth.getName(),
+                auth.getAuthorities().iterator().next().getAuthority());
+
+        return ResponseEntity.accepted().body(authResponse);
+    }
+
+}
---
+++ src/main/java/com/yely/bartrack_backend/auth/AuthDTOResponse.java
@@ -0,0 +1,5 @@
+package com.yely.bartrack_backend.auth;
+
+public record AuthDTOResponse(String message, String username, String roles) {
+
+}
