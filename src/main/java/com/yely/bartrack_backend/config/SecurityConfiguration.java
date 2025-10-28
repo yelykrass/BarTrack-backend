de0687234a372014a9cbce36d46601f0de4c9810
Yelyzaveta Krasiuk <lizakrasyuk24@gmail.com>
2025-10-28 12:18:00 +0100

    add cors config

--- src/main/java/com/yely/bartrack_backend/config/SecurityConfiguration.java
+++ src/main/java/com/yely/bartrack_backend/config/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.yely.bartrack_backend.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.yely.bartrack_backend.security.JpaUserDetailsService;
 
@@ -31,7 +36,7 @@ public class SecurityConfiguration {
         @Bean
         SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http
-                                .cors(withDefaults())
+                                .cors(cors -> cors.configurationSource(corsConfiguration()))
                                 .csrf(csrf -> csrf.disable())
                                 .headers(header -> header
                                                 .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
@@ -47,7 +52,7 @@ public class SecurityConfiguration {
 
                                                 .requestMatchers(HttpMethod.POST, endpoint + "/register")
                                                 .hasRole("ADMIN")
-                                                .requestMatchers(endpoint + "/login").permitAll()
+                                                .requestMatchers(endpoint + "/login").hasAnyRole("ADMIN", "USER")
                                                 .requestMatchers(HttpMethod.GET, endpoint + "/private").hasRole("ADMIN")
                                                 .anyRequest().authenticated())
                                 .userDetailsService(jpaUserDetailsService)
@@ -63,4 +68,17 @@ public class SecurityConfiguration {
                 return new BCryptPasswordEncoder();
         }
 
+        @Bean
+        CorsConfigurationSource corsConfiguration() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowCredentials(true);
+                configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+                configuration.setExposedHeaders(Arrays.asList("Authorization"));
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
 }
